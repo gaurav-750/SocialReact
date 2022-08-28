593af207b6f50bcabe27289bb89d8126751a68de
Gaurav <gauravsomani52750@gmail.com>
2022-08-28 15:23:01 +0530

    edit profile ui

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from '../styles/settings.module.css';
 import { useAuth } from '../hooks/index';
 
 const Settings = () => {
   const auth = useAuth();
+
+  const [editMode, setEditMode] = useState(false);
+  const [name, setName] = useState(auth.user?.name);
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [savingForm, setSavingForm] = useState(false);
+
+  const updateProfile = () => {};
 
   return (
     <div className={styles.settings}>
@@ -21,21 +29,65 @@ const Settings = () => {
 
       <div className={styles.field}>
         <div className={styles.fieldLabel}>Name</div>
-        <div className={styles.fieldValue}>{auth.user?.name}</div>
+        {editMode ? (
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        ) : (
+          <div className={styles.fieldValue}>{auth.user?.name}</div>
+        )}
       </div>
 
-      <div className={styles.field}>
-        <div className={styles.fieldLabel}>Password</div>
-        <input type="password"></input>
-      </div>
+      {editMode && (
+        <>
+          <div className={styles.field}>
+            <div className={styles.fieldLabel}>Password</div>
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            ></input>
+          </div>
 
-      <div className={styles.field}>
-        <div className={styles.fieldLabel}>Confirm Password</div>
-        <input type="password"></input>
-      </div>
+          <div className={styles.field}>
+            <div
+              className={styles.fieldLabel}
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+            >
+              Confirm Password
+            </div>
+            <input type="password"></input>
+          </div>
+        </>
+      )}
 
       <div className={styles.btnGrp}>
-        <button className={`button ${styles.editBtn}`}>Edit Profile</button>
+        {editMode ? (
+          <>
+            <button
+              className={`button ${styles.saveBtn}`}
+              onClick={updateProfile}
+            >
+              {savingForm ? 'Saving Profile..' : 'Save Profile'}
+            </button>
+            <button
+              className={`button ${styles.editBtn}`}
+              onClick={() => setEditMode(false)}
+            >
+              Go Back
+            </button>
+          </>
+        ) : (
+          <button
+            className={`button ${styles.editBtn}`}
+            onClick={() => setEditMode(true)}
+          >
+            Edit Profile
+          </button>
+        )}
       </div>
     </div>
   );
